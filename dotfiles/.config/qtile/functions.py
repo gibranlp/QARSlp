21281c93a2a739a80ccb516d624cefeaa199bca2
gibranlp <root@gibranlp.dev>
2023-06-26 09:28:04 -0600

    added emoji rofi

--- dotfiles/.config/qtile/functions.py
+++ dotfiles/.config/qtile/functions.py
@@ -216,21 +216,28 @@ secondary_color = secondary_pallete(color, differentiator)
 
 def i3lock_colors(qtile):
   subprocess.run(['i3lock', 
-    '--insidever-color={}'.format(secondary_color[5])+"22",
-    '--ringver-color={}'.format(color[2]),
-    '--insidewrong-color={}'.format(color[6]),
-    '--ringwrong-color={}'.format(color[3]),
-    '--inside-color={}'.format(secondary_color[5])+"22",
-    '--ring-color={}'.format(secondary_color[5])+"22",        
-    '--line-color={}'.format(color[2]),          
-    '--separator-color={}'.format(color[4]),   
-    '--verif-color={}'.format(color[7]),          
-    '--wrong-color=#880000bb',          
+    '--ring-color={}'.format(secondary_color[0])+"33",
+    '--inside-color={}'.format(secondary_color[0])+"33",
+    '--line-color={}'.format(color[2]),
+    '--separator-color={}'.format(color[4]),
     '--time-color={}'.format(color[2]),           
-    '--date-color={}'.format(color[3]),           
-    '--layout-color={}'.format(color[0]),         
-    '--keyhl-color={}'.format(color[1]),         
-    '--bshl-color={}'.format(color[3]),               
+    '--date-color={}'.format(color[4]),
+    '--insidever-color={}'.format(secondary_color[0])+"99",
+    '--ringver-color={}'.format(secondary_color[0])+"99",
+    '--verif-color={}'.format(color[7]),          
+    '--verif-text=Checking',
+    '--insidewrong-color={}'.format(secondary_color[0])+"DD",
+    '--ringwrong-color={}'.format(secondary_color[0])+"DD",
+    '--wrong-color={}'.format(color[1]),
+    '--wrong-text=Wrong!',
+            
+              
+       
+              
+                        
+    '--keyhl-color={}'.format(color[4]),         
+    '--bshl-color={}'.format(color[2]),
+                 
     '--clock',
     '--blur', '10',                 
     '--indicator',       
