d3b4ae6cce25eab2efaef8c17a69bb6e5e5cd639
gibranlp <root@gibranlp.dev>
2023-06-30 14:01:09 -0600

    updates

--- update.sh
+++ update.sh
@@ -35,10 +35,10 @@ function update(){
   cp -r ~/QARSlp/dotfiles/.config/rofi/* ~/.config/rofi/
   cp -r ~/QARSlp/dotfiles/.config/qtile/* ~/.config/qtile/
   # cp ~/QARSlp/dotfiles/.shortcuts ~/
-  cp -r ~/QARSlp/dotfiles/.local/bin/* ~/.local/bin
+  #cp -r ~/QARSlp/dotfiles/.local/bin/* ~/.local/bin
   chmod +x ~/.local/bin/*
   
-  # cp ~/QARSlp/dotfiles/.config/picom/picom.conf ~/.config/picom/picom.conf
+  cp ~/QARSlp/dotfiles/.config/picom/picom.conf ~/.config/picom/picom.conf
   # cp ~/QARSlp/dotfiles/.config/dunst/dunstrc ~/.config/wal/templates
   # cp ~/QARSlp/dotfiles/.config/cava/config ~/.config/wal/templates
   # cp ~/QARSlp/dotfiles/.config/ranger/rc.conf ~/.config/ranger/rc.conf
