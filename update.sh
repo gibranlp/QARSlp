0ba9be1844e928f5d1f14ab94a9f2858064bbe73
gibranlp <root@gibranlp.dev>
2023-07-10 08:09:21 -0600

    new changes

--- update.sh
+++ update.sh
@@ -13,6 +13,19 @@ function install_new_packages() {
    'xclip'
    'xdotool'
    'noto-fonts-emoji'
+   'unclutter'
+   'flameshot'
+   'bluez-utils'
+   'ripgrep'
+   'noto-fonts-emoji'
+   'fzf'
+  'cups'
+  'thefuck'
+  'pamixer'
+  'exa'
+  'xclip'
+  'xdotool'
+  'man'
 )
 
 for packet in "${packets[@]}"; do
@@ -24,6 +37,14 @@ done
 function aur_packages() {
   packets=(
     'rofi-emoji'
+    'insect'
+    'gromit-mpx-git'
+    'i3lock-fancy'
+    'zathura-pdf-mupdf' 
+    'zathura-pywal-git'
+    'zathura-ps'
+    'libby-git'
+    'python-rofi-git'
 )
 for packet in "${packets[@]}"; do
     echo "Instalando --> ${packet}"
@@ -32,19 +53,19 @@ done
 }
 
 function update(){
-  #cp -r ~/QARSlp/dotfiles/.config/rofi/* ~/.config/rofi/
+  cp -r ~/QARSlp/dotfiles/.config/rofi/* ~/.config/rofi/
   cp -r ~/QARSlp/dotfiles/.config/qtile/* ~/.config/qtile/
-  #cp ~/QARSlp/dotfiles/.shortcuts ~/
-  #cp -r ~/QARSlp/dotfiles/.local/bin/recorder ~/.local/bin/
-  #cp -r ~/QARSlp/dotfiles/.local/bin/notesfi ~/.local/bin/
-  #chmod +x ~/.local/bin/*
-  #cp ~/QARSlp/dotfiles/.shortcuts ~/
+  cp ~/QARSlp/dotfiles/.shortcuts ~/
+  cp -r ~/QARSlp/dotfiles/.local/bin/recorder ~/.local/bin/
+  cp -r ~/QARSlp/dotfiles/.local/bin/notesfi ~/.local/bin/
+  chmod +x ~/.local/bin/*
+  cp ~/QARSlp/dotfiles/.shortcuts ~/
   
   cp ~/QARSlp/dotfiles/.config/picom/picom.conf ~/.config/picom/picom.conf
   cp ~/QARSlp/dotfiles/.zshrc ~/
-  # cp ~/QARSlp/dotfiles/.config/dunst/dunstrc ~/.config/wal/templates
-  # cp ~/QARSlp/dotfiles/.config/cava/config ~/.config/wal/templates
-  # cp ~/QARSlp/dotfiles/.config/ranger/rc.conf ~/.config/ranger/rc.conf
+  cp ~/QARSlp/dotfiles/.config/dunst/dunstrc ~/.config/wal/templates
+  cp ~/QARSlp/dotfiles/.config/cava/config ~/.config/wal/templates
+  cp ~/QARSlp/dotfiles/.config/ranger/rc.conf ~/.config/ranger/rc.conf
   cp ~/QARSlp/dotfiles/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
   cp ~/QARSlp/dotfiles/.config/gromit-mpx.ini ~/.config
   cp ~/QARSlp/dotfiles/.oh-my-zsh/themes/avit.zsh-theme ~/.oh-my-zsh/themes
